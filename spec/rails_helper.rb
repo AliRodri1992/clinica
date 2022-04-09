f22014d5a08f868f04515af7b97d36cf7e5cf06d
Ali Rodriguez <ali.rodri.vasquez@gmail.com>
2022-04-09 13:56:09 -0500

    Style/StringLiterals: Prefer single-quoted strings when you don't need string interpolation or special symbols

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,7 @@ require 'capybara/rspec'
 ENV['RAILS_ENV'] ||= 'test'
 
 require_relative '../config/environment'
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 require 'support/factory_bot'
 require 'support/database_cleaner'
