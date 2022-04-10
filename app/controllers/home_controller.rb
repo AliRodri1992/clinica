b5d6669aba7130dd345e83429fd9b6796410d298
Ali Rodriguez <ali.rodri.vasquez@gmail.com>
2022-04-09 23:58:55 -0500

    add info tiles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,3 @@
 class HomeController < ApplicationController
-  before_action :add_breadcrumbs_bulma, only: %i[index]
   def index; end
-
-  private
-
-  def add_index_breadcrumb_metronic
-    add_breadcrumbs_bulma 'Home'.html_safe, root_path
-  end
 end
